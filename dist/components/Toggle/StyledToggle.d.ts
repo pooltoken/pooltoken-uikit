a48c60f67cbc7d8c88880ab4fb0247c171ef66a3
pooltoken <pooltokenfinance@gmail.com>
2021-02-27 11:40:54 +0100

    build

--- dist/components/Toggle/StyledToggle.d.ts
+++ dist/components/Toggle/StyledToggle.d.ts
@@ -1,4 +1,4 @@
-import { ToggleProps, HandleProps, InputProps } from "./types";
+import { HandleProps, InputProps, ToggleProps } from "./types";
 export declare const Handle: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, HandleProps, never>;
 export declare const Input: import("styled-components").StyledComponent<"input", import("styled-components").DefaultTheme, InputProps, never>;
 declare const StyledToggle: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, ToggleProps, never>;
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -164,7 +164,7 @@ var StyledButton = styled.button(templateObject_1$2 || (templateObject_1$2 = __m
     return (fullWidth ? "100%" : "max-content");
 }, function (_a) {
     var size = _a.size;
-    return (size === "sm" ? "32px" : "48px");
+    return (size === "sm" ? "32px" : "42px");
 }, function (_a) {
     var size = _a.size;
     return (size === "sm" ? "0 16px" : "0 24px");
@@ -787,7 +787,7 @@ var getScale = function (_a) {
             return "24px";
         case scales.MD:
         default:
-            return "32px";
+            return "28px";
     }
 };
 var Checkbox = styled.input.attrs({ type: "checkbox" })(templateObject_1$f || (templateObject_1$f = __makeTemplateObject(["\n  appearance: none;\n  overflow: hidden;\n  cursor: pointer;\n  position: relative;\n  display: inline-block;\n  height: ", ";\n  width: ", ";\n  vertical-align: middle;\n  transition: background-color 0.2s ease-in-out;\n  border: 0;\n  border-radius: 0px;\n  background-color: ", ";\n  box-shadow: ", ";\n\n  &:after {\n    content: \"\";\n    position: absolute;\n    border-bottom: 2px solid;\n    border-left: 2px solid;\n    border-color: transparent;\n    top: 30%;\n    left: 0;\n    right: 0;\n    width: 50%;\n    height: 25%;\n    margin: auto;\n    transform: rotate(-50deg);\n    transition: border-color 0.2s ease-in-out;\n  }\n\n  &:hover:not(:disabled):not(:checked) {\n    box-shadow: ", ";\n  }\n\n  &:focus {\n    outline: none;\n    box-shadow: ", ";\n  }\n\n  &:checked {\n    background-color: ", ";\n    &:after {\n      border-color: white;\n    }\n  }\n\n  &:disabled {\n    cursor: default;\n    opacity: 0.6;\n  }\n"], ["\n  appearance: none;\n  overflow: hidden;\n  cursor: pointer;\n  position: relative;\n  display: inline-block;\n  height: ", ";\n  width: ", ";\n  vertical-align: middle;\n  transition: background-color 0.2s ease-in-out;\n  border: 0;\n  border-radius: 0px;\n  background-color: ", ";\n  box-shadow: ", ";\n\n  &:after {\n    content: \"\";\n    position: absolute;\n    border-bottom: 2px solid;\n    border-left: 2px solid;\n    border-color: transparent;\n    top: 30%;\n    left: 0;\n    right: 0;\n    width: 50%;\n    height: 25%;\n    margin: auto;\n    transform: rotate(-50deg);\n    transition: border-color 0.2s ease-in-out;\n  }\n\n  &:hover:not(:disabled):not(:checked) {\n    box-shadow: ", ";\n  }\n\n  &:focus {\n    outline: none;\n    box-shadow: ", ";\n  }\n\n  &:checked {\n    background-color: ", ";\n    &:after {\n      border-color: white;\n    }\n  }\n\n  &:disabled {\n    cursor: default;\n    opacity: 0.6;\n  }\n"])), getScale, getScale, function (_a) {
@@ -1297,13 +1297,13 @@ var scaleKeyValues = {
         toggleWidth: "36px",
     },
     md: {
-        handleHeight: "32px",
-        handleWidth: "32px",
+        handleHeight: "22px",
+        handleWidth: "22px",
         handleLeft: "4px",
         handleTop: "4px",
-        checkedLeft: "calc(100% - 36px)",
-        toggleHeight: "40px",
-        toggleWidth: "72px",
+        checkedLeft: "calc(100% - 26px)",
+        toggleHeight: "30px",
+        toggleWidth: "56px",
     },
 };
 var getScale$2 = function (property) { return function (_a) {
@@ -1695,8 +1695,8 @@ var shadows = {
 var spacing = [0, 4, 8, 16, 24, 32, 48, 64];
 var radii = {
     small: "4px",
-    default: "16px",
-    card: "32px",
+    default: "0px",
+    card: "0px",
     circle: "50%",
 };
 var zIndices = {
